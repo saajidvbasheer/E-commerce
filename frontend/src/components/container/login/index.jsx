86b41290e5176612098c488be26c8e96c4c66aca
saajidvbasheer <saajidvbasheer@gmail.com>
2023-05-13 23:56:27 +0530

    added login page

--- frontend/src/components/container/login/index.jsx
+++ frontend/src/components/container/login/index.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 const Login = () => {
+    
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(email);
+    }
     return (
-        <h1>
-            Login
-        </h1>
+        <div className="login-cont">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label for="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Email" id="email" name="email"></input>
+                <label for="password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="********" id="password" name="password"></input>
+                <button type="submit">Log In</button>     
+            </form>
+        </div>
+        
     )
 }
 
