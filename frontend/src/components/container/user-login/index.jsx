5b0d1b4edb47276b34ae89c231cf32704e7bf4b3
saajidvbasheer <saajidvbasheer@gmail.com>
2023-05-14 14:55:06 +0530

    added user login

--- frontend/src/components/container/user-login/index.jsx
+++ frontend/src/components/container/user-login/index.jsx
@@ -15,12 +15,12 @@ const Login = (props) => {
     return (
         <div className="login-cont">
             <form className="login-form" onSubmit={handleSubmit}>
-                <label for="email">Email</label>
+                <label for="User email">Email</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Email" id="email" name="email"></input>
                 <label for="password">Password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="********" id="password" name="password"></input>
                 <button type="submit">login</button>
-                <button onClick={() => props.onFormSWitch('user-login')}>User Login</button>
+                <button onClick={() => props.onFormSWitch('login')}>admin Login</button>
             </form>
         </div>
         
